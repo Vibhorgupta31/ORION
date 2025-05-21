6fde7d7b7781a5a977a88c77dd872dd4ebb32887
Vibhor Gupta <vibhorgupta7@gmail.com>
2025-05-21 14:40:58 -0400

    Use parent logger, correct spelling mistakes / typos, change edge attributes, add SourceDataLoader properties

--- parsers/ClinGenVariantPathogenicity/src/loadClinGenVariantPathogenicity.py
+++ parsers/ClinGenVariantPathogenicity/src/loadClinGenVariantPathogenicity.py
@@ -3,7 +3,7 @@ import csv
 from Common.extractor import Extractor
 from Common.loader_interface import SourceDataLoader
 from biolink_constants import PRIMARY_KNOWLEDGE_SOURCE, NODE_TYPES, SEQUENCE_VARIANT
-from Common.prefixes import HGNC  # only an example, use existing curie prefixes or add your own to the prefixes file
+from Common.prefixes import HGNC
 from Common.utils import GetData
 from Common.utils import LoggingUtil
 from datetime import date
@@ -18,9 +18,9 @@ class ClinGenVariantPathogenicityLoader(SourceDataLoader):
     provenance_id: str = "infores:clingen"
     # increment parsing_version whenever changes are made to the parser that would result in changes to parsing output
     parsing_version: str = "1.0"
-    has_sequence_variants = (
-        True  # Flag to use robokop_genetics server to tackle sequence variant data
-    )
+    # source_data
+    source_data_url: str = "http://erepo.clinicalgenome.org/evrepo/api/classifications/all?format=tabbed"
+    has_sequence_variants = True  # Flag to use robokop_genetics server to tackle sequence variant data
 
     def __init__(self, test_mode: bool = False, source_data_dir: str = None):
         """
@@ -67,76 +67,70 @@ class ClinGenVariantPathogenicityLoader(SourceDataLoader):
                 ),  # predicate extractor
                 lambda line: {
                     NODE_TYPES: SEQUENCE_VARIANT,
-                    "Variation": line["#Variation"],
-                    "HGNC_Gene_Symbol": line["HGNC Gene Symbol"],
+                    "VARIATION": line["#Variation"],
+                    "HGNC_GENE_SYMBOL": line["HGNC Gene Symbol"],
                 },  # subject properties
                 lambda line: {},  # object properties
                 lambda line: {
                     PRIMARY_KNOWLEDGE_SOURCE: self.provenance_id,
-                    "Assertion": line["Assertion"],
-                    "Mode_Of_Inheritance": moi_normalizer(
+                    "ASSERTION": line["Assertion"],
+                    "MODE_OF_INHERITANCE": self.moi_normalizer(
                         line["Mode of Inheritance"],
                         line["Evidence Repo Link"],
                     ),
-                    "Applied_Evidence_Codes_Met": line["Applied Evidence Codes (Met)"],
-                    "Applied_Evidence_Codes_Not_Met": line[
+                    "APPLIED_EVIDENCE_CODES_MET": line["Applied Evidence Codes (Met)"],
+                    "APPLIED_EVIDENCE_CODES_NOT_MET": line[
                         "Applied Evidence Codes (Not Met)"
                     ],
-                    "Summary": line["Summary of interpretation"],
-                    "Pubmed_Articles": line["PubMed Articles"],
-                    "Expert_Panel": line["Expert Panel"],
-                    "Evidence_Repo_Link": line["Evidence Repo Link"],
-                    "Guideline": line["Guideline"],
-                    "Approval_Date": line["Approval Date"],
-                    "Published_Date": line["Published Date"],
-                    **get_edge_properties(line["Assertion"]),
+                    "SUMMARY": line["Summary of interpretation"],
+                    "PUBMED_ARTICLES": line["PubMed Articles"],
+                    "EXPERT_PANEL": line["Expert Panel"],
+                    "EVIDENCE_REPO_LINK": line["Evidence Repo Link"],
+                    "GUIDELINE": line["Guideline"],
+                    "APPROVAL_DATA": line["Approval Date"],
+                    "PUBLISHED_DATE": line["Published Date"],
+                    **self.get_edge_properties(line["Assertion"]),
                 },  # edge properties
             )
         return extractor.load_metadata
 
+    moi_lookup = {
+        "Autosomal dominant inheritance": "HP:0000006",
+        "Autosomal dominant inheritance (with paternal imprinting (HP:0012274))": "HP:0012274",
+        "Autosomal dominant inheritance (mosaic)": ["HP:0000006", "HP:0001442"],
+        "Autosomal recessive inheritance": "HP:0000007",
+        "Autosomal recessive inheritance (with genetic anticipation)": ["HP:0000007"],
+        "X-linked inheritance": "HP:0001417",
+        "X-linked inheritance (dominant (HP:0001423))": "HP:0001423",
+        "X-linked inheritance (recessive (HP:0001419))": "HP:0001419",
+        "Semidominant inheritance": "HP:0032113",
+        "Mitochondrial inheritance": "HP:0001427",
+        "Mitochondrial inheritance (primarily or exclusively heteroplasmic)": "HP:0001427",
+        # No HPO term for heteroplasmic type
+    }
 
-# Supporting functions for processing the data
+    def moi_normalizer(self, MOI, EREPO_LINK):
+        MOI = str(MOI)
+        try:
+            HPO = self.moi_lookup[MOI]
+        except KeyError:
+            self.logger.warning(
+                f"We do not have a mapping for {MOI=} in the moi_lookup dictionary at source {EREPO_LINK}"
+            )
+            HPO = ""
+        # TODO: Check the second HPO for "Autosomal recessive inheritance (with genetic anticipation)" and "Mitochondrial inheritance (primarily or exclusively heteroplasmic)"
+        return {"label": MOI, "HPO": HPO}
 
-# Logging
-logger = LoggingUtil.init_logging(
-    "ORION.parsers.ClinGenVariantPathogenicityLoader",
-    log_file_path=os.environ["ORION_LOGS"],
-)
-
-moi_lookup = {
-    "Autosomal dominant inheritance": "HP:0000006",
-    "Autosomal dominant inheritance (with paternal imprinting (HP:0012274))": "HP:0012274",
-    "Autosomal dominant inheritance (mosaic)": ["HP:0000006", "HP:0001442"],
-    "Autosomal recessive inheritance": "HP:0000007",
-    "Autosomal recessive inheritance (with genetic anticipation)": ["HP:0000007"],
-    "X-linked inheritance": "HP:0001417",
-    "X-linked inheritance (dominant (HP:0001423))": "HP:0001423",
-    "X-linked inheritance (recessive (HP:0001419))": "HP:0001419",
-    "Semidominant inheritance": "HP:0032113",
-    "Mitochondrial inheritance": "HP:0001427",
-    "Mitochondrial inheritance (primarily or exclusively heteroplasmic)": "HP:0001427",  # No HPO term for heteroplasmic type
-}
-
-
-def moi_normalizer(MOI, EREPO_LINK):
-    MOI = str(MOI)
-    try:
-        HPO = moi_lookup[MOI]
-    except KeyError:
-        logger.warning(
-            f"We do not have a mapping for {MOI=} in the moi_lookup dictionary at source {EREPO_LINK}"
-        )
-        HPO = ""
-    # TODO: Check the second HPO for "Autosomal recessive inheritance (with genetic anticipation)" and "Mitochondrial inheritance (primarily or exclusively heteroplasmic)"
-    return {"label": MOI, "HPO": HPO}
-
-
-def get_edge_properties(assertion):
-    if assertion == "Benign" or assertion == "Likely Benign":
-        return {"direction": "Contradicts", "negated": True}
-    elif assertion == "Likely Pathogenic" or assertion == "Pathogenic":
-        return {"direction": "Supports", "negated": False}
-    elif assertion == "Uncertain Significance":
-        return {"direction": "Inconclusive", "negated": True}
-    else:
-        return {"Status": "Not evaluated", "direction": "Inconclusive", "negated": True}
+    def get_edge_properties(self, assertion):
+        if assertion == "Benign" or assertion == "Likely Benign":
+            return {"direction": "Contradicts", "negated": True}
+        elif assertion == "Likely Pathogenic" or assertion == "Pathogenic":
+            return {"direction": "Supports", "negated": False}
+        elif assertion == "Uncertain Significance":
+            return {"direction": "Inconclusive", "negated": True}
+        else:
+            return {
+                "Status": "Not evaluated",
+                "direction": "Inconclusive",
+                "negated": True,
+            }
